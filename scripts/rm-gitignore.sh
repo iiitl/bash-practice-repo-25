b553934a96acaf2d14fec9eb6e65bf566fb99d0b
Kishan8548 <surendrakuma123m@gmail.com>
2025-03-29 11:11:30 +0530

    Remove files listed in .gitignore for all Git repos

--- scripts/rm-gitignore.sh
+++ scripts/rm-gitignore.sh
@@ -2,3 +2,10 @@
 
 # Removes all files written in .gitignore of any git repository (multiple) present inside current directory
 # To find list of all git repos inside current directory you can use: "find . -name .git -type d -prune"
+
+find . -name .git -type d -prune | while read git_path; do
+    project_root=$(dirname "$git_path")
+    [ -f "$project_root/.gitignore" ] && xargs rm -f < "$project_root/.gitignore"
+done
+
+echo "Gitignore cleanup completed."
